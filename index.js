783c40dbf3ce395fc3392f283c134c46f43f421f
Salman053 <salmankhanm859@gmail.com>
2024-12-26 14:30:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();  
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
