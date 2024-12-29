a262ced11277ef3cbcefc5b74a749a29038799b9
Salman053 <salmankhanm859@gmail.com>
2024-12-29 20:27:03 +0500

    changes in the indec

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-// configuration of the cors
+
+// Configuration of CORS
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -15,44 +16,30 @@ app.use(
   })
 );
 
-// configuration of the express json to except the json from data
+// Parse JSON requests
 app.use(
   express.json({
     limit: "30kb",
   })
 );
-//configuration of the express urlencoded connection
+
+// Parse URL-encoded requests
 app.use(express.urlencoded({ extended: true, limit: "30kb" }));
 
-app.use(express.json());
-
-// configuration of the static files
+// Serve static files
 app.use(express.static("public"));
 
-// configuration of the cookie-parser to send secure cookies
+// Cookie Parser
 app.use(cookieParser());
 
+// Connect to the database
+connectDatabase().catch((error) => {
+  console.error(
+    `MongoDB connection failed. Ensure your connection string is correct. ERROR: ${error}`
+  );
+});
 
-
-connectDatabase()
-  .then(() => {
-    app.listen(process.env.PORT || 6000, () => {
-      console.log(
-        `*#____#* Server is listening on Port ${process.env.PORT} *****Happy Coding*****`
-      );
-    });
-  })
-  .catch((error) => {
-    console.log(
-      `mongoDB error while connection the connection is failed ERROR : ${error}`
-    );
-  });
-
-// routes
-
-
-
-//import Routes
+// Routes
 import studentRoutes from "./routes/student.routes.js";
 
 app.use("/api/v1/student", studentRoutes);
@@ -61,3 +48,5 @@ app.get("/api/users", (req, res) => {
   res.send("Hello");
 });
 
+// Export the app for Vercel
+export default app;
